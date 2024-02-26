d92741c871f863d09e1608df1b4d25c0e3c3a34a
samKoSphere <139430653+samKoSphere@users.noreply.github.com>
2024-02-26 15:48:35 +0800

    first push

---
+++ components/infinite-scroll.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef, useCallback } from "react";
+
+interface InfiniteScrollProps {
+  loadMore: () => void;
+}
+
+const InfiniteScroll = ({ loadMore }: InfiniteScrollProps) => {
+  const loaderRef = useRef<HTMLDivElement | null>(null);
+
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const target = entries[0];
+      if (target.isIntersecting) {
+        loadMore();
+      }
+    },
+    [loadMore]
+  );
+
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 0,
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+    if (loaderRef.current) observer.observe(loaderRef.current);
+
+    return () => {
+      if (loaderRef.current) observer.unobserve(loaderRef.current);
+    };
+  }, [handleObserver]);
+
+  return <div ref={loaderRef} className="h-10"></div>;
+};
+
+export default InfiniteScroll;
---
+++ components/pagination/pagination.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+interface PaginationProps {
+  currentPage: number; // 當前頁碼
+  totalPages: number; // 總頁數
+  onPageChange: (page: number) => void; // 頁面變化時的回調函數
+}
+
+const Pagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: PaginationProps) => {
+  return (
+    <div className="flex justify-end items-center space-x-4">
+      <button
+        disabled={currentPage <= 1}
+        onClick={() => onPageChange(currentPage - 1)}
+        className="px-4 py-2  text-white rounded disabled:opacity-50"
+      >
+        &lt;
+      </button>
+      <span>
+        Page {currentPage} / {totalPages}
+      </span>
+      <button
+        disabled={currentPage >= totalPages}
+        onClick={() => onPageChange(currentPage + 1)}
+        className="px-4 py-2  text-white rounded disabled:opacity-50"
+      >
+        &gt;
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
